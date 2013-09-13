98977ed819e5d569ca356a8af68c3da2919e7d13
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2013-09-13 13:15:31 -0400

    Removing unnecessary call to 'istanbul report' (issue #8).

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,52 +67,28 @@ module.exports = function (grunt) {
         function () {
             var done = this.async();
             
-            var tasks = [
-                    function (next) {
-                        grunt.log.writeln("Generating coverage report...");
-                        grunt.util.spawn(
-                            {
-                                args : [ "report", "html" ],
-                                cmd  : "istanbul",
-                                opts : { stdio: "inherit" }
-                            },
-                            next
-                        );
-                    },
-                    function (next) {
-                        grunt.log.writeln(
-                            "Checking test coverage thresholds..."
-                        );
-                        grunt.util.spawn(
-                            {
-                                args : [
-                                    "check-coverage", "--statements", "100",
-                                    "--functions", "100", "--branches", "100",
-                                    "--lines", "100"
-                                ],
-                                cmd  : "istanbul",
-                                opts : { stdio: "inherit" }
-                            },
-                            next
-                        );
+            grunt.log.writeln("Checking test coverage thresholds...");
+            grunt.util.spawn(
+                {
+                    args : [
+                        "check-coverage", "--statements", "100",
+                        "--functions", "100", "--branches", "100",
+                        "--lines", "100"
+                    ],
+                    cmd  : "istanbul",
+                    opts : { stdio: "inherit" }
+                },
+                function (error) {
+                    if (error) {
+                        grunt.log.error("Some code is not covered.");
+                        return grunt.fail.fatal(error);
                     }
-                ];
-                
-            (function next (error) {
-                var task = tasks.shift();
-                
-                if (!error && task) {
-                    return task(next);
+                    else {
+                        grunt.log.ok("All code has test coverage.");
+                        return done();
+                    }
                 }
-                else if (error) {
-                    grunt.log.error("Some code is not covered.");
-                    return grunt.fail.fatal(error);
-                }
-                else {
-                    grunt.log.ok("All code has test coverage.");
-                    return done();
-                }
-            })();
+            );
         }
     );
     
@@ -139,12 +115,12 @@ module.exports = function (grunt) {
             function (error) {
                 if (error) {
                     grunt.log.error("Some tests failed.");
-                    grunt.fail.fatal(error);
+                    return grunt.fail.fatal(error);
                 }
                 else {
                     grunt.log.ok("All tests passed.");
                 }
-                done();
+                return done();
             }
         );
     });
