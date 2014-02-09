4892fddf13cd3b097753a5256abf1a17aeb37bb7
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-02-08 20:37:51 -0500

    Error handling for server start and stop.

--- test/Server_spec.js
+++ test/Server_spec.js
@@ -1,5 +1,6 @@
 var expect = require("chai").expect;
 var Server = require("../lib/Server");
+var sinon  = require("sinon");
 
 // Mocha seems to have a bug that causes unhandled exceptions in an event
 // handler to not be handled as test errors.
@@ -26,15 +27,13 @@ describe("A server", function () {
 		});
 
 		afterEach(function (done) {
-			server
-				.stop()
-				.fail(function () { /* Ignore errors. */ })
-				.nodeify(done);
+			server.stop()
+			.fail(function () { /* Ignore errors. */ })
+			.nodeify(done);
 		});
 
 		it("can be started", function (done) {
-			server
-			.start()
+			server.start()
 			.then(function (port) {
 				expect(port, "listening port")
 					.to.be.a("number")
@@ -43,10 +42,45 @@ describe("A server", function () {
 			.nodeify(done);
 		});
 
-		it("can be stopped", function (done) {
-			var stop = server.stop.bind(server);
+		it("fails to start if already listening", function (done) {
+			server.start()
+			.then(function () {
+				return server.start();
+			})
+			.then(
+				function () {
+					throw new Error("Start should not succeed if already listeing.");
+				},
+				function (error) {
+					expect(error, "start error")
+						.to.be.an.instanceOf(Error)
+						.and.to.have.property("message", "Server is already listening.");
+				}
+			)
+			.nodeify(done);
+		});
 
-			server.start().then(stop).nodeify(done);
+		it("can be stopped", function (done) {
+			server.start()
+			.then(function () {
+				return server.stop();
+			})
+			.nodeify(done);
+		});
+
+		it("fails to stop if not listening", function (done) {
+			server.stop()
+			.then(
+				function () {
+					throw new Error("Stop should not succeed if not listening.");
+				},
+				function (error) {
+					expect(error, "stop error")
+						.to.be.an.instanceOf(Error)
+						.and.to.have.property("message", "Server is not listening.");
+				}
+			)
+			.nodeify(done);
 		});
 
 		it("can callback when the server starts", function (done) {
@@ -58,18 +92,41 @@ describe("A server", function () {
 				done();
 			}
 
-			server.start(callback);
+			server.start(callback).done();
+		});
+
+		it("calls back with an error if the server fails to start", function (done) {
+			function callback (error) {
+				expect(error, "error argument").to.be.an.instanceOf(Error);
+				done();
+			}
+
+			server.start()
+			.then(function () {
+				return server.start(callback);
+			});
 		});
 
 		it("can callback when the server stops", function (done) {
 			function callback (error) {
-				expect(error).not.to.exist;
+				expect(error, "error argument").not.to.exist;
 				done();
 			}
 
-			var stop = server.stop.bind(server, callback);
+			server.start()
+			.then(function () {
+				return server.stop(callback);
+			})
+			.done();
+		});
 
-			server.start().then(stop);
+		it("calls back with an error if the server fails to stop", function (done) {
+			function callback (error) {
+				expect(error, "error argument").to.be.an.instanceOf(Error);
+				done();
+			}
+
+			server.stop(callback);
 		});
 
 		it("emits the 'ready' event once it has started", function (done) {
@@ -81,21 +138,69 @@ describe("A server", function () {
 				},
 				done
 			));
-			server.start();
+			server.start().done();
+		});
+
+		it("emits the 'error' event if the server fails to start", function (done) {
+			var errorHandler = sinon.spy();
+
+			server.on("error", errorHandler);
+
+			server.start()
+			.then(function () {
+				expect(errorHandler.called, "initially called").to.be.false;
+				return server.start();
+			})
+			.fail(function (error) {
+				if (error.message.indexOf("already listening") === -1) {
+					throw error;
+				}
+				else {
+					expect(errorHandler.calledOnce, "finally called").to.be.true;
+					expect(errorHandler.args[0], "event arguments").to.have.length(1);
+					expect(errorHandler.args[0][0], "error argument").to.be.an.instanceOf(Error);
+				}
+			})
+			.nodeify(done);
 		});
 
 		it("emits the 'stopped' event after it has stopped", function (done) {
-			var stop = server.stop.bind(server);
-
 			server.on("stopped", done);
-			server.start().then(stop);
+
+			server.start()
+			.then(function () {
+				return server.stop();
+			})
+			.done();
+		});
+
+		it("emits the 'error' event if the server fails to stop", function (done) {
+			var errorHandler = sinon.spy();
+
+			server.on("error", errorHandler);
+
+			server.start()
+			.then(function () {
+				return server.stop();
+			})
+			.then(function () {
+				expect(errorHandler.called, "initially called").to.be.false;
+				return server.stop();
+			})
+			.fail(function (error) {
+				if (error.message.indexOf("not listening") === -1) {
+					throw error;
+				}
+				else {
+					expect(errorHandler.calledOnce, "finally called").to.be.true;
+					expect(errorHandler.args[0], "event arguments").to.have.length(1);
+					expect(errorHandler.args[0][0], "error argument").to.be.an.instanceOf(Error);
+				}
+			})
+			.nodeify(done);
 		});
 
 	});
-
-	it("cannot be started if already listening");
-
-	it("cannot be stopped if not listening");
 
 	it("binds to an arbitrary port by default");
 
